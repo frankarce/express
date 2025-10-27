f68cf5916c568e615fcc5fa5e9084d2e2eface8e
frankarce <francisco.j.arce@gmail.com>
2025-10-27 10:43:03 -0700

    Adds JWT authentication and token handling
    Integrates JWT for user registration by generating and sending access tokens in cookies after successful signup.
    
    Improves security and enables stateless authentication for future API requests.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
- 
+ export const TOKEN_SECRET = 'Crea tu propia clave';
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,17 +1,28 @@
 import User from '../models/user.models.js';
 import bcrypt from 'bcryptjs';
+import { createAccessToken } from '../libs/jwt.sign.js';
+import jwt from 'jsonwebtoken';
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
     //User.create({ name, email, password });
     try {
         const passwordHash = await bcrypt.hash(password, 10);
         const newUser = new User({ name, email, password : passwordHash });
-        await newUser.save();
+        const userSaved = await newUser.save();
+        const token = await createAccessToken({ id: userSaved._id });
+        res.cookie('token', token);
+       
+        res.json({
+            id: userSaved._id,
+            name: userSaved.name,
+            email: userSaved.email
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: 'Error al registrar el usuario' });
     }
-    res.send('registrado'); 
+    //res.cookie('token', token);
+    //res.json({ message: 'Usuario registrado correctamente' });
 };
 
 
